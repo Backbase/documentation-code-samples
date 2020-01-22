cab0d281da23177286c1a2af5c04e288f7a91b2f
SteveWyatt74 <49515646+SteveWyatt74@users.noreply.github.com>
2020-01-22 16:03:04 +0000

    Add tags to include annotations in asciidoc

--- service-sdk/9.0.0/create-persistence/example-service/src/main/java/com/backbase/example/Application.java
+++ service-sdk/9.0.0/create-persistence/example-service/src/main/java/com/backbase/example/Application.java
@@ -7,12 +7,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// tag::application-annotations[]
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaRepositories
 @EntityScan
 public class Application extends SpringBootServletInitializer {
-
+// end::application-annotations[]
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
