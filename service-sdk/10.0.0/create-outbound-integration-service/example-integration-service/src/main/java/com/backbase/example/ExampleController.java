4ffc12461d55271ddd0180c93fcb4adf7dc06d92
Stephen Wyatt <stephen.wyatt@backbase.com>
2020-02-07 10:39:38 +0000

    Add documentation code samples for 10.0.0 line

---
+++ service-sdk/10.0.0/create-outbound-integration-service/example-integration-service/src/main/java/com/backbase/example/ExampleController.java
@@ -0,0 +1,41 @@
+package com.backbase.example;
+
+import com.backbase.buildingblocks.presentation.errors.BadRequestException;
+import com.backbase.buildingblocks.presentation.errors.InternalServerErrorException;
+import com.backbase.example.integration.MessageClient;
+import com.backbase.example.mapper.IntegrationMessageMapper;
+import com.backbase.example.model.Message;
+import com.backbase.integration.example.rest.spec.v1.example.ExampleApi;
+import com.backbase.integration.example.rest.spec.v1.example.IntegrationGetResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.util.List;
+
+@RestController
+public class ExampleController implements ExampleApi {
+
+    @Autowired
+    private MessageClient messageClient;
+
+    // tag::getIntegration[]
+    @Override
+    public List<IntegrationGetResponseBody> getIntegration(HttpServletRequest httpServletRequest,
+                                                     HttpServletResponse httpServletResponse)
+            throws BadRequestException, InternalServerErrorException {
+
+        List<Message> messages = null;
+        try {
+            messages = messageClient.getMessages();
+        } catch (IOException e) {
+            throw new InternalServerErrorException("Error has occurred sending message");
+        }
+
+        return IntegrationMessageMapper.INSTANCE.messagesToIntegrationGetResponseBodys(messages);
+    }
+    // end::getIntegration[]
+}
---
+++ service-sdk/10.0.0/create-outbound-integration-service/example-integration-spec/src/main/resources/docs/api.md
@@ -0,0 +1,7 @@
+# Example Integration Service API Spec API Documentation
+
+Currently the Example Integration Service API Spec API contains the following operations:
+* Create an item or Update an item
+* Get an item by ID
+* Get list of items, optionally filtering by name
+* Delete item
