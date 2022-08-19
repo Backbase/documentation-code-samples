658d994e084a29e6b42b8329a91225b0c6ef7777
Marcin Mazurek <63855724+bbzurek@users.noreply.github.com>
2022-08-19 09:17:44 +0100

    SSDK 15.0.0 (#52)
    * 15.0.0 major release uplift
    - refactored for ssdk 15 and java 17
    - removed raml
    - removed references to junit4
    - unique project names to build from root pom
    
    * fixed gitleaks to v1.6.0

---
+++ service-sdk/15.0.0/service-to-service-communication/example-service-to-service/src/main/java/com/backbase/example/service/GreetingsServiceImpl.java
@@ -0,0 +1,47 @@
+package com.backbase.example.service;
+
+import com.backbase.example.domain.Greeting;
+import com.backbase.example.mapper.GreetingsMapper;
+import com.backbase.example.repository.GreetingsRepository;
+import com.backbase.integration.example.client.v1.MessageApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GreetingsServiceImpl implements GreetingsService {
+
+    @Autowired
+    private GreetingsRepository greetingsRepository;
+
+    @Autowired
+    private MessageApi exampleIntegrationApiClient;
+
+    @Override
+    public List<Greeting> getGreetings() {
+        return greetingsRepository.findAll();
+    }
+
+    // tag::getExternalGreetings[]
+    @Override
+    public List<Greeting> getExternalGreetings() {
+        return GreetingsMapper.INSTANCE.integrationMessageToGreeting(exampleIntegrationApiClient.getMessages());
+    }
+    // end::getExternalGreetings[]
+
+    @Override
+    public Greeting getGreetingById(String id) {
+        return greetingsRepository.findById(id).get();
+    }
+
+    @Override
+    public void saveGreeting(Greeting greeting) {
+        greetingsRepository.save(greeting);
+    }
+
+    @Override
+    public void deleteGreeting(Greeting greeting) {
+        greetingsRepository.delete(greeting);
+    }
+}
